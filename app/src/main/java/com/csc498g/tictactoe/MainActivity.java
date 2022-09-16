4a8c070649f4a8fa985914999bfd7b5a98e9fca0
Simon <moawadsimon@gmail.com>
2022-09-16 12:17:22 +0300

    Refactored the HashMap into an ArrayList. Added some casts

--- app/src/main/java/com/csc498g/tictactoe/MainActivity.java
+++ app/src/main/java/com/csc498g/tictactoe/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Random;
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     final int[] board = new int[9]; // The board of the game
     int[][] win = new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}}; // The winning states
     boolean player; // true is player 1 (yellow) and false is player 2 / ai (red)
-    final HashMap<Integer, ImageView> views = new HashMap<>(); // The images
+    final ArrayList<ImageView> views = new ArrayList<>(); // The images
     boolean ai = true; // Whether 2 Players are playing
     MiniMax algorithm = new MiniMax(0, 1, -1, board, win); // The minimax algorithm
 
@@ -26,15 +27,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        views.put(0, findViewById(R.id.piece0));
-        views.put(1, findViewById(R.id.piece1));
-        views.put(2, findViewById(R.id.piece2));
-        views.put(3, findViewById(R.id.piece3));
-        views.put(4, findViewById(R.id.piece4));
-        views.put(5, findViewById(R.id.piece5));
-        views.put(6, findViewById(R.id.piece6));
-        views.put(7, findViewById(R.id.piece7));
-        views.put(8, findViewById(R.id.piece8));
+        views.add((ImageView) findViewById(R.id.piece0));
+        views.add((ImageView) findViewById(R.id.piece1));
+        views.add((ImageView) findViewById(R.id.piece2));
+        views.add((ImageView) findViewById(R.id.piece3));
+        views.add((ImageView) findViewById(R.id.piece4));
+        views.add((ImageView) findViewById(R.id.piece5));
+        views.add((ImageView) findViewById(R.id.piece6));
+        views.add((ImageView) findViewById(R.id.piece7));
+        views.add((ImageView) findViewById(R.id.piece8));
 
         start();
 
@@ -59,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         for(int i = 0; i < this.board.length; i++) {
             board[i] = -1;
         }
-        views.values().forEach(t -> t.setImageResource(0));
+        views.forEach(t -> t.setImageResource(0));
 
         if(ai) {
 
@@ -92,14 +93,15 @@ public class MainActivity extends AppCompatActivity {
                 player = !player;
             }
 
+            if(algorithm.isWin() != -1) {
+                Toast.makeText(getApplicationContext(), "Player " + (algorithm.isWin() + 1) + " wins", Toast.LENGTH_LONG).show();
+            } else if(!Arrays.stream(board).anyMatch(t -> t == -1)) {
+                Toast.makeText(getApplicationContext(), "Tie", Toast.LENGTH_LONG).show();
+            }
 
         }
 
-        if(algorithm.isWin() != -1) {
-            Toast.makeText(getApplicationContext(), "Player " + (algorithm.isWin() + 1) + " wins", Toast.LENGTH_LONG).show();
-        } else if(!Arrays.stream(board).anyMatch(t -> t == -1)) {
-            Toast.makeText(getApplicationContext(), "Tie", Toast.LENGTH_LONG).show();
-        }
+
 
     }
 
